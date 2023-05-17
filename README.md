8dbb73548cc0ff8f3cc08fd8e25b581201db1dc6
Rashed Zaman <jmrashed@gmail.com>
2023-05-17 14:48:28 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Draw polyline on google map using latitude and longitude coordinates
+
+This project demonstrates how to draw a polyline on Google Maps using latitude and longitude coordinates. It also includes marking the start and end points on the map.
+
+## Getting Started
+
+These instructions will help you set up the project on your local machine for development and testing purposes.
+
+### Prerequisites
+
+- Google Maps JavaScript API key. Get an API key by following the [Google Maps JavaScript API documentation](https://developers.google.com/maps/documentation/javascript/get-api-key).
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/jmrashed/google-maps-polyline-draw.git
+   ```
+
+2. Open `assets\js\script.js` in a text editor.
+3. Replace `YOUR_API_KEY` with your actual Google Maps JavaScript API key obtained in the prerequisites step:
+
+```javascript
+function loadMapScript() {
+  var script = document.createElement("script");
+  script.src =
+    "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap";
+  document.head.appendChild(script);
+}
+```
+
+# Usage
+
+1. Open index.html in a web browser.
+2. The map will be displayed with a polyline connecting the given coordinates.
+3. The start and end points will be marked on the map with custom icons and labels.
+
+
+# Demo 
+**Demo in the web browser**
+<img src="./assets/readme/demo1.png">
+
+
+
+**Demo in the json file**
+<img src="./assets/readme/demo2.png">
+
+
+**Demo in the script file**
+<img src="./assets/readme/demo3.png">
+
+
+
+**Demo in the web browser**
+
+
+# Customization
+
+You can customize the project by modifying the following:
+
+- Coordinates: In `script.js`, update the coordinates array with your desired latitude, longitude, label, and time.
+- Polyline Style: In `script.js`, modify the properties of polyline object to change the polyline's color, opacity, and weight.
+- Marker Icons: In `script.js`, update the URL of the start and end marker icons to use your own icons.
+- Marker Labels: In `script.js`, adjust the label text and color for the start and end markers.
+
+
+# Contributing
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+# License
+This project is licensed under the MIT License.
---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+// Load the Google Maps JavaScript API asynchronously
+function loadMapScript() {
+  var script = document.createElement("script");
+  script.src =
+    "https://maps.googleapis.com/maps/api/js?key=AIzaSyBVF8ZCdPLYBEC2-PCRww1_Q0Abe5GYP1c&callback=initMap";
+  document.head.appendChild(script);
+}
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 23.773481433356284, lng: 90.41602417477222 },
+    zoom: 12,
+  });
+
+  // Load coordinates from the JSON file
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var coordinates = JSON.parse(xhr.responseText);
+
+        var polyline = new google.maps.Polyline({
+          path: coordinates,
+          geodesic: true,
+          strokeColor: "#FF0000",
+          strokeOpacity: 1.0,
+          strokeWeight: 2,
+          map: map,
+        });
+
+        // Add location point labels with time
+        for (var i = 0; i < coordinates.length; i++) {
+          var marker = new google.maps.Marker({
+            position: coordinates[i],
+            label: {
+              text: coordinates[i].label + "\n" + coordinates[i].time,
+              color: "white",
+            },
+            map: map,
+          });
+        }
+
+        // Mark the start point with a custom icon
+        var startPoint = coordinates[0];
+        var startMarker = new google.maps.Marker({
+          position: startPoint,
+          icon: {
+            url: "./assets/images/green-dot.png",
+            labelOrigin: new google.maps.Point(12, 10),
+          },
+          label: {
+            text: "Start",
+            color: "white",
+          },
+          map: map,
+        });
+
+        // Mark the end point with a custom icon
+        var endPoint = coordinates[coordinates.length - 1];
+        var endMarker = new google.maps.Marker({
+          position: endPoint,
+          icon: {
+            url: "./assets/images/yellow-dot.png",
+            labelOrigin: new google.maps.Point(12, 10),
+          },
+          label: {
+            text: "End",
+            color: "white",
+          },
+          map: map,
+        });
+      } else {
+        console.error("Failed to load coordinates from JSON file.");
+      }
+    }
+  };
+  xhr.open("GET", "./assets/json/coordinates.json");
+  xhr.send();
+}
+// Call the function to load the map script
+loadMapScript();
